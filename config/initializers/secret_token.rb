dc228be18516f539def0cf87725b5528c2005217
Marc <marcseifert@Marcs-Mac-Mini.local>
2015-02-05 08:20:30 +0800

    Initial backbone blog app commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneBlog::Application.config.secret_token = '5437d257f2aee2754bcc3234fa887bfc3525d91371a159a040725adff3ad5362ba0e3efe4887b472542b60bfea3c74cb8727407f7460c343184054eb3de10338'
