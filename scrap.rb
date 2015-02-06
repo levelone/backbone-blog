af0b04b11f85fb50989113e03a878453c8aa1e6a
Marc <marcseifert@Marcs-Mac-Mini.local>
2015-02-06 18:09:33 +0800

    Scrap added to fetch in personal computer for more info.

---
+++ scrap.rb
@@ -0,0 +1,16 @@
+# Reference for console commands:
+# http://documentcloud.github.com/backbone/
+#
+# Commands incase Server won't run!
+# rm <path_name>/server.pid
+# ps aux | grep rails
+# kill -9 <rails_pid>
+
+
+
+
+# fetch() grabs the default set of models from the server
+@collection.fetch()
+
+# reset() replaces a collection with a new list of models (refreshes/updates collection)
+@collection.reset()
